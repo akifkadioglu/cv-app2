ab5dd23e87ecdc255ef388427a5d66f94401e81a
akif kadıoğlu <akif.kadioglu.28@gmail.com>
2023-02-10 17:06:26 +0300

    icon sizes changed

--- src/views/Home/Components/Informations.vue
+++ src/views/Home/Components/Informations.vue
@@ -3,7 +3,7 @@
     <div class="text-center text-h5 mt-3 text-f">Akif Kadıoğlu</div>
     <v-icon
       class="ml-5 mr-5 mt-2 mb-2"
-      size="40"
+      :size="isMobile ? 25 : 40"
       v-for="(item, index) in talents"
       :key="index"
     >
@@ -17,12 +17,13 @@ export default {
   name: "Home-Informations",
   data() {
     return {
+      isMobile: this.$functions.isMobile(),
+
       talents: [
         this.$icons.GO,
         this.$icons.VUE,
         this.$icons.FLUTTER,
         this.$icons.JAVASCRIPT,
-        this.$icons.PHP,
         this.$icons.LARAVEL,
       ],
     };
